52b8bbfe0aa9017f1886f1c37e02ca3e72a1fde6
Samuel Lord <sam@sllord.info>
2022-05-20 14:42:07 -0500

    Create brunchdate.sh

---
+++ brunchdate.sh
@@ -0,0 +1,24 @@
+releases_url="https://dl.google.com/dl/edgedl/chromeos/recovery/recovery.json"
+UPDATEID=$RANDOM
+echo This update has an update code of $UPDATEID
+cd ~/Downloads
+mkdir "UPDATE_$UPDATEID"
+cd "UPDATE_$UPDATEID"
+
+curl $releases_url -o releases.json 
+string="$(grep -o '"url":.*/chromeos_.*_'${board}'_.*.bin\..*"' recovery.json | sort -V | tail -n 1)"
+info_string="$(grep -A 6 --max-count 1 "$string" ./recovery.json)"
+build_num="$(echo "$info_string" | grep --max-count 1 -o '"version":.*' | sed 's/^[[:blank:]]*//' | cut -d " " -f 2- | tr -d '",')"
+build_url="$(echo "$string" | sed 's/^[[:blank:]]*//' | cut -d " " -f 2- | tr -d '",')"
+chrome_version=$(echo $info_string | cut -d "." -f 9 | cut -d '"' -f 7)
+brunch_download=$(curl https://api.github.com/repos/sebanc/brunch/releases | grep browser_download_url | grep $chrome_version | tail -n1 |  cut -f
+ 3 | sed 's/        "browser_download_url"://' | sed 's/"//' | sed "s/\n//")
+
+curl $brunch_download -o brunch.tar.gz
+curl $build_url -o update.zip
+binfiles=(*.bin)
+update="${binfiles[0]}"
+sudo chromeos-update -r $update -f brunch.tar.gz
+
+cd ..
+echo "Brunch update script done."
